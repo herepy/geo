3d9187d4000a820a50679e58ddd23e846de8a0df
pengyu <917103429@qq.com>
2019-08-13 15:16:19 +0800

    添加驱动基类，实现一些共用方法

---
+++ src/driver/BaseDriver.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pengyu
+ * Date: 2019/8/13
+ * Time: 15:08
+ */
+
+namespace Pengyu\Geo\Driver;
+
+class BaseDriver implements DriverInterface
+{
+    public function init(array $config){}
+
+    public function add(string $name, float $lon, float $lat): bool{}
+
+    public function bulk(array $points): bool{}
+
+    public function del(string $name): bool{}
+
+    public function flush(): bool{}
+
+    public function distanceFrom(string $name1, string $name2, string $unit = "m"): float{}
+
+    public function radiusFrom(string $name, int $limit=10, string $unit = "m"): float{}
+
+    protected function checkPoint(float $lon,float $lat):bool
+    {
+        if ($lat > self::GEO_LAT_RANG[1] || $lat < self::GEO_LAT_RANG[0] || $lon > self::GEO_LON_RANG[1] || $lon < self::GEO_LON_RANG[0]) {
+            return false;
+        }
+
+        return true;
+    }
+
+}
