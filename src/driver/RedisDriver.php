c71f3e1432a229809a34c343b1ce039ada9ab51c
pengyu <917103429@qq.com>
2019-08-13 15:58:38 +0800

    基本功能实现

--- src/driver/RedisDriver.php
+++ src/driver/RedisDriver.php
@@ -8,40 +8,80 @@
 
 namespace Pengyu\Geo\Driver;
 
-class RedisDriver implements DriverInterface
+class RedisDriver extends BaseDriver
 {
+    protected $instance;
+    protected $key;
+
     public function init(array $config)
     {
+        $host=isset($config["host"]) ? $config["host"] : "localhost";
+        $port=isset($config["port"]) ? $config["port"] : "6379";
+        $db=isset($config["db"]) ? $config["db"] : 0;
 
+        $redis=new \Redis();
+        $redis->connect($host,$port);
+
+        if (isset($config["password"])) {
+            $redis->auth($config["password"]);
+        }
+        $redis->select($db);
+
+        $this->key=isset($config["key"]) ? $config["key"] : "geo_collection";
+        $this->instance=$redis;
     }
 
     public function add(string $name, float $lon, float $lat): bool
     {
-        // TODO: Implement add() method.
+        if (!$this->checkPoint($lon,$lat)) {
+            return false;
+        }
+
+        $this->instance->geoAdd($this->key,$lon,$lat,$name);
+        return true;
     }
 
     public function bulk(array $points): bool
     {
-        // TODO: Implement bulk() method.
+        $flag=true;
+        $this->instance->multi();
+
+        foreach ($points as $item) {
+            if (!$this->checkPoint($item["lon"],$item["lat"])) {
+                $flag=false;
+                break;
+            }
+
+            $this->instance->geoAdd($this->key,$item["lon"],$item["lat"],$item["name"]);
+        }
+
+        if (!$flag) {
+            $this->instance->discard();
+            return false;
+        }
+
+        $this->instance->exec();
+        return true;
     }
 
     public function del(string $name): bool
     {
-        // TODO: Implement del() method.
+        return $this->instance->zRem($this->key,$name) ? true : false;
     }
 
     public function flush(): bool
     {
-        // TODO: Implement flush() method.
+        return $this->instance->del($this->key) ? true : false;
     }
 
     public function distanceFrom(string $name1, string $name2, string $unit = "m"): float
     {
-        // TODO: Implement distanceFrom() method.
+        return $this->instance->geoDist($this->key,$name1,$name2,$unit);
     }
 
-    public function radiusFrom(string $name, int $limit, string $unit = "m"): float
+    public function radiusFrom(string $name,float $distance,string $unit="m",int $limit=10): float
     {
-        // TODO: Implement radiusFrom() method.
+
+        return $this->instance->rawCommand("GEORADIUSBYMEMBER",$this->key,$name,$distance,$unit,"WITHDIST","COUNT",$limit);
     }
 }
