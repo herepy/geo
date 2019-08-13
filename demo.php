9fa6be0ca8859ab8b102d8fb2108400107296e4d
pengyu <917103429@qq.com>
2019-08-13 16:26:52 +0800

    测试距离

---
+++ demo.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pengyu
+ * Date: 2019/8/13
+ * Time: 16:21
+ */
+
+use Pengyu\Geo\GeoClient;
+
+require_once "vendor/autoload.php";
+
+$config=[
+    "host"      =>  "localhost",
+    "port"      =>  6379,
+    "db"        =>  0,
+    "key"       =>  "test_geo",
+    "password"  =>  "1993918py"
+];
+
+$client=GeoClient::build($config);
+$client->add("chengdu",104.07,30.67);
+$client->add("deyang",104.38,31.13);
+
+echo $client->distanceFrom("chengdu","deyang");
