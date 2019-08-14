82bb4557fbfc77da774ec107c77a1b462694c7b7
pengyu <917103429@qq.com>
2019-08-14 15:11:00 +0800

    测试

--- demo.php
+++ demo.php
@@ -11,18 +11,21 @@ use Pengyu\Geo\GeoClient;
 require_once "vendor/autoload.php";
 
 $config=[
+    "driver"    =>  "mongodb",
     "host"      =>  "localhost",
-    "port"      =>  6379,
-    "db"        =>  0,
-    "key"       =>  "test_geo",
-    "password"  =>  "1993918py"
+    "port"      =>  "27017",
 ];
 
 $client=GeoClient::build($config);
+$client->flush();
 $client->add("chengdu",104.07,30.67);
 $client->add("deyang",104.38,31.13);
 $client->add("mianyang",104.67,31.46);
 
+$client->bulk([
+    ["name"=>"test1","lon"=>104.21,"lat"=>30.21],
+    ["name"=>"test2","lon"=>105.12,"lat"=>31.69],
+    ["name"=>"test3","lon"=>105.36,"lat"=>30.28],
+]);
 
-echo $client->distanceFrom("chengdu","mianyang","km")."\n";
-var_dump($client->radiusFrom("chengdu",100,"km"));
+echo $client->distanceFrom("chengdu","mianyang","km");
