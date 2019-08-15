4f876bb8ab6a331f6e8fd6c6d06d4396b2f2ea19
pengyu <917103429@qq.com>
2019-08-15 17:40:08 +0800

    测试elasticsearch驱动

--- demo.php
+++ demo.php
@@ -11,21 +11,25 @@ use Pengyu\Geo\GeoClient;
 require_once "vendor/autoload.php";
 
 $config=[
-    "driver"    =>  "mongodb",
-    "host"      =>  "localhost",
-    "port"      =>  "27017",
+    "driver"    =>  "elasticsearch",
+//    "host"      =>  "localhost",
+//    "port"      =>  "27017",
+    "index"     =>  "geo",
+    "type"      =>  "test"
 ];
 
 $client=GeoClient::build($config);
-$client->flush();
-$client->add("chengdu",104.07,30.67);
-$client->add("deyang",104.38,31.13);
-$client->add("mianyang",104.67,31.46);
+//$client->flush();
+//$client->add("chengdu",104.07,30.67);
+//$client->add("deyang",104.38,31.13);
+//$client->add("mianyang",104.67,31.46);
+//
+//$client->bulk([
+//    ["name"=>"test1","lon"=>104.21,"lat"=>30.21],
+//    ["name"=>"test2","lon"=>105.12,"lat"=>31.69],
+//    ["name"=>"test3","lon"=>105.36,"lat"=>30.28],
+//]);
 
-$client->bulk([
-    ["name"=>"test1","lon"=>104.21,"lat"=>30.21],
-    ["name"=>"test2","lon"=>105.12,"lat"=>31.69],
-    ["name"=>"test3","lon"=>105.36,"lat"=>30.28],
-]);
-
-echo $client->distanceFrom("chengdu","deyang");
+//echo $client->distanceFrom("chengdu","deyang");
+$result=$client->radiusFrom("deyang",300,\Pengyu\Geo\Driver\DriverInterface::GEO_UNIT_KM);
+var_dump($result);
